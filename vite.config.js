22f8453725c55e23d6e74d4fe58a316a20dda5a0
Yair Orchen <yairorchen1@gmail.com>
2022-12-14 19:24:31 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
